aeed42dc70b4622499d2c7c6025c338a49d88c3f
harishma <harishmahari1502gmail.com>
2017-07-12 12:38:04 +0530

    sprint10 submited

---
+++ content/jquery/Admin_Delete.js
@@ -0,0 +1,13 @@
+﻿$(document).on('click', '#btn_delete', function () {
+    var id = $(this).data("id");
+    var email = $("#email").val();
+   
+    $.ajax({
+        type: 'GET',
+        dataType: 'Json',
+        url: 'Home/admin_delete',
+        data: { loginid: id, emailaddress: email },
+        success: function (data) {
+        }
+    })
+});
